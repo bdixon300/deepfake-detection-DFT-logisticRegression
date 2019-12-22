1c41f6d1ebe56ddb28d9af619b8757ea1b051170
Ben Dixon <ben.dixon300@gmail.com>
2019-12-22 17:10:02 +0000

    Add functionality for evaluating logistic regression model

--- main.py
+++ main.py
@@ -7,25 +7,39 @@ from torchvision import transforms, utils
 import torch.nn as nn
 import matplotlib.pyplot as plt
 import collections
+import numpy as np
 
 
-# Initializing dataset
-data_set = DeepFakeSmallDataset(csv_file='training_labels.csv',
+# Fake : 0
+# Real : 1
+
+# Initializing datasets
+dataset = DeepFakeSmallDataset(csv_file='training_labels.csv',
                                 root_dir='frames_fft/',
-                                transform=transforms.ToTensor())
-                                
-# See dataset samples
-"""plt.imshow(data_set[0]['image'], cmap='gray')
-print(data_set[0]['label'])
-plt.pause(0.01)
-input("<Hit Enter To Close>")"""
+                                transform=transforms.ToTensor())                              
 
-#train_sampler, val_sampler, test_sampler = 
-#train_sampler = SubsetRandomSampler(training_indices)
-train_loader = DataLoader(data_set,
-                          batch_size=4,
-                          shuffle=False, #FIXME: SHUFFLING to true breaks it
-                          num_workers=4 # 1 for CUDA
+# Creating data indices for training and validation splits:
+dataset_size = len(dataset)
+indices = list(range(dataset_size))
+split = int(np.floor(0.6 * dataset_size))
+random_seed=52
+if True:
+    np.random.seed(random_seed)
+    np.random.shuffle(indices)
+train_indices, test_indices = indices[split:], indices[:split]
+
+
+train_loader = DataLoader(dataset,
+                          batch_size=32,
+                          num_workers=4, # 1 for CUDA
+                          sampler=SubsetRandomSampler(train_indices)
+                         # pin_memory=True # CUDA only
+                         )
+
+test_loader = DataLoader(dataset,
+                          batch_size=32,
+                          num_workers=4, # 1 for CUDA,
+                          sampler=SubsetRandomSampler(test_indices)
                          # pin_memory=True # CUDA only
                          )
 
@@ -35,7 +49,7 @@ class LogisticRegressionModel(nn.Module):
         super(LogisticRegressionModel, self).__init__()
         self.linear = nn.Linear(input_dim, output_dim)
     def forward(self, x):
-        out = F.softmax(self.linear(x))
+        out = self.linear(x)
         return out
 
 input_dim = 281*281
@@ -48,12 +62,14 @@ optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 # Train model
 iter = 0
-num_epochs = 100
+n_iters = 30000
+num_epochs = int(n_iters / (len(dataset) / 32))
+
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
 
         # Load images as Variable
-        images = torch.autograd.Variable(images.view(-1, 281*281))
+        images = torch.autograd.Variable(images.view(-1, input_dim))
         labels = torch.autograd.Variable(labels.long())
 
         # Clear gradients w.r.t. parameters
@@ -71,19 +87,26 @@ for epoch in range(num_epochs):
         # Updating parameters
         optimizer.step()
         iter += 1
-        
+
         # Evaluate logistic regression model 
-        #TODO
-        """if iter%500==0:
+        if iter%500==0:
             # calculate Accuracy
             correct = 0
             total = 0
             for images, labels in test_loader:
-                images = Variable(images.view(-1, 28*28))
+                images = torch.autograd.Variable(images.view(-1, input_dim))
                 outputs = model(images)
                 _, predicted = torch.max(outputs.data, 1)
-                total+= labels.size(0)
+                total += labels.size(0)
                 # for gpu, bring the predicted and labels back to cpu fro python operations to work
-                correct+= (predicted == labels).sum()
+                correct += (predicted == labels).sum()
+
+            # Compute confusion matrix
+            confusion_vector = predicted / labels
+            true_positives = torch.sum(confusion_vector == 1).item()
+            false_positives = torch.sum(confusion_vector == float('inf')).item()
+            true_negatives = torch.sum(torch.isnan(confusion_vector)).item()
+            false_negatives = torch.sum(confusion_vector == 0).item()
+            print("TP: {} FP: {} TN: {} FN: {}".format(true_positives, false_positives, true_negatives, false_negatives))
             accuracy = 100 * correct/total
-            print("Iteration: {}. Loss: {}. Accuracy: {}.".format(iter, loss.item(), accuracy))"""
+            print("Iteration: {}. Loss: {}. Accuracy: {}.".format(iter, loss.item(), accuracy))
